fac40ef964d1ffa7d73900411dad6c6cca1eb9d5
Sourabh Bharamkar <bharamkarsourabh8989@gmail.com>
2023-08-05 00:06:24 +0530

    added component  Expenses ( container for expense items )

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
-import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 
 function App() {
-  const expenses = [
-    {
-      id: 1,
-      date: new Date(2023, 5, 16),
-      title: 'Car Insurance',
-      amount: 500,
-      location: 'Kolhapur'
-    },
-    {
-      id: 2,
-      date: new Date(2023, 6, 29),
-      title: 'Movie',
-      amount: 400,
-      location: 'Kolhapur'
-    },
-    {
-      id: 2,
-      date: new Date(2023, 7, 15),
-      title: 'Electric Bill',
-      amount: 500,
-      location: 'Mumbai'
-    },
-    {
-      id: 2,
-      date: new Date(2023, 8, 1),
-      title: 'Food',
-      amount: 500,
-      location: 'Kagal'
-    }
-  ]
 
-  const expenseItems = expenses.map((expense) => {
-    return <ExpenseItem title={expense.title} date={expense.date} amount={expense.amount} location={expense.location}></ExpenseItem>
-  })
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      {expenseItems}
+      <Expenses></Expenses>
     </div>
   );
 }
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,51 @@
+import Card from "../UI/Card";
+import './Expenses.css'
+import ExpenseItem from "./ExpenseItem";
+
+const Expenses = () => {
+    const expenses = [
+        {
+            id: 1,
+            date: new Date(2023, 5, 16),
+            title: 'Car Insurance',
+            amount: 500,
+            location: 'Kolhapur'
+        },
+        {
+            id: 2,
+            date: new Date(2023, 6, 29),
+            title: 'Movie',
+            amount: 400,
+            location: 'Kolhapur'
+        },
+        {
+            id: 2,
+            date: new Date(2023, 7, 15),
+            title: 'Electric Bill',
+            amount: 500,
+            location: 'Mumbai'
+        },
+        {
+            id: 2,
+            date: new Date(2023, 8, 1),
+            title: 'Food',
+            amount: 500,
+            location: 'Kagal'
+        }
+    ]
+
+    const expenseItems = expenses.map((expense) => {
+        return <ExpenseItem title={expense.title} date={expense.date} amount={expense.amount} location={expense.location}></ExpenseItem>
+    })
+
+
+    return (
+        <Card className="expenses">
+            {expenseItems}
+        </Card>
+    )
+
+
+}
+
+export default Expenses;
