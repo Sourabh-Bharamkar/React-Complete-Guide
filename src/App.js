70fccd56b71bc0f6d8b47a5557877158c0b5ffc2
Sourabh Bharamkar <bharamkarsourabh8989@gmail.com>
2023-08-14 02:12:04 +0530

    performance optimisation -adding keys to list elements

--- src/App.js
+++ src/App.js
@@ -3,22 +3,60 @@ import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 
+const dummyExpenses = [
+  {
+    id: 1,
+    date: new Date(2023, 5, 16),
+    title: 'Car Insurance',
+    amount: 500,
+    location: 'Kolhapur'
+  },
+  {
+    id: 2,
+    date: new Date(2023, 6, 29),
+    title: 'Movie',
+    amount: 400,
+    location: 'Kolhapur'
+  },
+  {
+    id: 2,
+    date: new Date(2023, 7, 15),
+    title: 'Electric Bill',
+    amount: 500,
+    location: 'Mumbai'
+  },
+  {
+    id: 2,
+    date: new Date(2023, 8, 1),
+    title: 'Food',
+    amount: 500,
+    location: 'Kagal'
+  },
+
+]
+
 function App() {
 
-  const [newExpense,setNewExpense]=useState('');
+  const [expenses, setExpenses] = useState(dummyExpenses);
+
+
 
   const addExpenseHandler = (expense) => {
+
     console.log('In app js');
     console.log(expense);
-    setNewExpense(expense) ;
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
+
   }
 
-  
+
   return (
     <div>
       <h2>Let's get started!</h2>
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses newExpense={newExpense}></Expenses>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,51 +9,14 @@ const Expenses = (props) => {
 
     const [filteredYear, setFilteredYear] = useState('2023');
 
-    const expenses = [
-        {
-            id: 1,
-            date: new Date(2023, 5, 16),
-            title: 'Car Insurance',
-            amount: 500,
-            location: 'Kolhapur'
-        },
-        {
-            id: 2,
-            date: new Date(2023, 6, 29),
-            title: 'Movie',
-            amount: 400,
-            location: 'Kolhapur'
-        },
-        {
-            id: 2,
-            date: new Date(2023, 7, 15),
-            title: 'Electric Bill',
-            amount: 500,
-            location: 'Mumbai'
-        },
-        {
-            id: 2,
-            date: new Date(2023, 8, 1),
-            title: 'Food',
-            amount: 500,
-            location: 'Kagal'
-        },
-
-    ]
-
-    if (props.newExpense) {
-        expenses.push(props.newExpense)
-    }
-
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear)
 
     }
 
-    const expenseItems = expenses.map((expense) => {
-        return <ExpenseItem title={expense.title} date={expense.date} amount={expense.amount} location={expense.location}></ExpenseItem>
+    const expenseItems = props.items.map((expense) => {
+        return <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount} location={expense.location}></ExpenseItem>
     })
-
 
     return (
         <Card className="expenses">
