7f4acbc120e2fc1e6084359a8987c73324355386
Sourabh Bharamkar <bharamkarsourabh8989@gmail.com>
2023-08-13 23:33:14 +0530

    Added filter for expenses

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import ExpensesFilter from './components/Expenses/ExpensesFilter'
 
 function App() {
 
 
   const [newExpense,setNewExpense]=useState('');
+  const [filteredYear,setFilteredYear]=useState('2023')
 
 
   const addExpenseHandler = (expense) => {
@@ -14,10 +16,16 @@ function App() {
     setNewExpense(expense) ;
   }
 
+  const filterChangeHandler=(selectedYear)=>{
+    setFilteredYear(selectedYear)
+
+  }
+
   return (
     <div>
       <h2>Let's get started!</h2>
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></ExpensesFilter>
       <Expenses newExpense={newExpense}></Expenses>
     </div>
   );
